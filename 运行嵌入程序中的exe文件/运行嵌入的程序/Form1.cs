4485c148b700e3234251cd47f05efcb651950a65
tkzc00 <2238983030@qq.com>
2023-04-03 11:06:48 +0800

    运行嵌入程序中的exe文件

---
+++ 运行嵌入程序中的exe文件/运行嵌入的程序/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+using 运行嵌入的程序.Properties;
+
+namespace 运行嵌入的程序
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = RunExe.RunExeRes(Resources.运行嵌入程序中的exe文件);
+            if (s != "OK")
+            {
+                MessageBox.Show(s);
+            }
+        }
+    }
+}
---
+++ 运行嵌入程序中的exe文件/运行嵌入的程序/RunExe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace 运行嵌入的程序
+{
+    internal class RunExe
+    {
+        public static string RunExeRes(Byte[] resFileName)
+        {
+            return RunExeRes(resFileName, null);
+        }
+
+        public static string RunExeRes(Byte[] resFileName, string[] args)
+        {
+            try
+            {
+                // 如果不用object[]将args包装，将会报“参数计算出错”的异常
+                object[] objs = new object[] { args };
+                Assembly asm = Assembly.Load(resFileName);
+                MethodInfo info = asm.EntryPoint;
+                ParameterInfo[] parameters = info.GetParameters();
+                if (args != null)
+                {
+                    if (parameters != null && parameters.Length > 0)
+                    {
+                        info.Invoke(null, objs);
+                    }
+                    else
+                    {
+                        info.Invoke(null, null);
+                    }
+                }
+                else
+                {
+                    info.Invoke(null, null);
+                }
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
