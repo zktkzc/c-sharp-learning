d234a0cea67f99dd549e8cf12ce644aab7cfd0bb
tkzc00 <2238983030@qq.com>
2023-04-03 11:45:40 +0800

    生成和识别二维码

---
+++ 生成和识别二维码/生成和识别二维码/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace 生成和识别二维码
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "二维码内容：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(796, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(930, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(569, 69);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(452, 78);
+            this.btnRead.TabIndex = 4;
+            this.btnRead.Text = "读取二维码";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(569, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "读取的二维码的内容：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(572, 194);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(449, 299);
+            this.textBox2.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(569, 511);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(452, 58);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "保存二维码";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 600);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("宋体", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "生成和识别二维码";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ 生成和识别二维码/生成和识别二维码/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using com.google.zxing;
+using com.google.zxing.common;
+
+namespace 生成和识别二维码
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 生成二维码
+        /// </summary>
+        /// <param name="sContent">内容</param>
+        /// <param name="iWidth">宽度</param>
+        /// <param name="iHeight">高度</param>
+        /// <returns>返回生成的二维码图片</returns>
+        public static Bitmap GetBitmap(string sContent, int iWidth, int iHeight)
+        {
+            string _message = sContent;
+            ByteMatrix byteMatrix = new MultiFormatWriter().encode(_message, BarcodeFormat.QR_CODE, iWidth, iHeight);
+            int _width = byteMatrix.Width;
+            int _height = byteMatrix.Height;
+            Bitmap bmap = new Bitmap(_width, _height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            for (int i = 0; i < _width; i++)
+            {
+                for (int j = 0; j < _height; j++)
+                {
+                    bmap.SetPixel(i, j, byteMatrix.get_Renamed(i, j) != -1 ? ColorTranslator.FromHtml("0xff000000") : ColorTranslator.FromHtml("0xffffffff"));
+                }
+            }
+            return bmap;
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            pictureBox1.Image = GetBitmap(textBox1.Text, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void btnSave_Click(object sender, System.EventArgs e)
+        {
+            pictureBox1.Image.Save(@"./test.jpg");
+        }
+
+        private void btnRead_Click(object sender, System.EventArgs e)
+        {
+            Image _img = Image.FromFile(@"./test.jpg");
+            string s = QrcodeToText((Bitmap)_img);
+            textBox2.Text = s;
+        }
+
+        /// <summary>
+        /// 识别二维码
+        /// </summary>
+        /// <param name="bitmap">需要识别的二维码</param>
+        /// <returns>识别出的二维码内容</returns>
+        public static string QrcodeToText(Bitmap bitmap)
+        {
+            LuminanceSource source = new RGBLuminanceSource(bitmap, bitmap.Width, bitmap.Height);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
+            try
+            {
+                Result result = new MultiFormatReader().decode(binaryBitmap);
+                return result.Text;
+            }
+            catch (Exception ex)
+            {
+                return string.Format("识别二维码出错！\r\n\r\n提示：{0}", ex.Message);
+            }
+        }
+    }
+}
